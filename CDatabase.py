2ef37fd635adbc83f973eec59666c1d4f9de0d14
neo <576582046@qq.com>
2019-01-17 11:10:35 +0800

    添加数据库查询和插入

--- CDatabase.py
+++ CDatabase.py
@@ -15,7 +15,7 @@ class CDatabase():
                 port=3306,
                 user='root',
                 password='root',
-                db='chatroom',
+                db='virus',
                 charset='utf8'
             )
 
@@ -25,12 +25,36 @@ class CDatabase():
             # 添加变量
             self.cursor = cursor
             print("数据库链接成功！")
-
-
-
         except Exception as ex:
             print(ex)
 
+
+
+    #查询操作
+    def query(self , sql_select , param=None):
+        try:
+            #self.cursor.execute("reset query cahce")
+            self.cursor.execute(sql_select , param)
+
+            #获取符合要求的数据列表
+            result = self.cursor.fetchall()
+
+            return result
+        except:
+            print("Error: unable to fecth data")
+            return None
+
+    #插入操作
+    def insert(self, sql_select, param=None):
+        try:
+            self.cursor.execute(sql_select, param)
+            self.connecter.commit()               # 提交数据
+            return True
+        except:
+            print("Error: insert failed")
+            # 发生错误时回滚
+            # self.cursor.rollback()
+            return None
 
 #测试数据库是否连接成功
 if __name__ == '__main__':
--- CServer.py
+++ CServer.py
@@ -8,6 +8,14 @@ import struct
 from CDatabase import *
 from enum import Enum
 
+#回复消息枚举
+class EnumMessageType(Enum):
+    GetVirusMD5 = 1
+    UploadVirus = 2
+
+
+
+
 
 class CServerSocket():
     #数据包放数据的总大小 = 2048
@@ -70,14 +78,60 @@ class CServerSocket():
 
     #从数据库中获取病毒的MD5值
     def __GetVirusMD5__(socket,  message, MsgSize):
-
         #从数据库中查询出病毒的MD5值
+        sql_select = "select virus_name , md5 from virus_list"
+        result = CServerSocket.db.query(sql_select)
 
+        #设置发送给客户端的消息
+        message_type = EnumMessageType.GetVirusMD5
 
-        pass
+        #构造发送给客户端的数据包
+        send_msg = ""
+        for row in result:
+            #send_msg += row[0] + ":";
+            send_msg += row[1] + "|";
+        #除去最后的|
+        send_msg = send_msg[:-1].encode('gbk')
+
+        #发送消息给客户端
+        return_msg = struct.pack("ii2048s" , len(send_msg) , message_type.value , send_msg)
+        socket.send(return_msg)
+
     #上次病毒到数据库
     def __UploadVirus__(socket,  message, MsgSize):
-        pass
+        #解析出用户上传上来的md5值
+        msgbuff, = struct.unpack('%ds' % MsgSize, message[8:8 + MsgSize])
+        virus_md5 = msgbuff.decode("gbk")
+
+        #从数据库中查询出病毒的MD5值
+        sql_select = "select md5 from virus_list"
+        result = CServerSocket.db.query(sql_select)
+
+        message_type = EnumMessageType.UploadVirus
+
+        sql_select = "INSERT INTO virus_list(md5) VALUES(%s)"
+
+
+        flag = 1
+        #判断数据库是否存在病毒
+        for row in result:
+            if virus_md5 == row[0]:
+                flag = 0
+                break;
+
+        if flag:
+            result = CServerSocket.db.insert(sql_select, virus_md5)
+
+
+
+        send_msg = "上传成功".encode('gbk')
+        return_msg = struct.pack("ii2048s", len(send_msg), message_type.value, send_msg)
+        socket.send(return_msg)
+
+
+
+
+
     # 类变量
     # 处理各种消息的函数
     dictFun = {
